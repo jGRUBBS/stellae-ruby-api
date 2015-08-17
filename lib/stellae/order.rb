e1431adf269d2dfa9aaa15c678f32e55c88b3fcf
Justin Grubbs <justin@jgrubbs.net>
2015-08-17 15:33:47 -0400

    add additional fields to order and properly add cdata

--- lib/stellae/order.rb
+++ lib/stellae/order.rb
@@ -6,14 +6,23 @@ module Stellae
 
         xml.ohn :"xmlns:b" => SCHEMA[:datacontract], :"xmlns:i" => SCHEMA[:instance] do
           xml.b :SERVICE,            order[:shipping_code]
-          xml.b :USER1,              "<![CDATA[#{order[:invoice_url]}]]>"
+          xml.b :USER1 do
+            xml.cdata!(order[:invoice_url])
+          end
           xml.b :CARRIER,            order[:carrier]
+          xml.b :CURRENCY,           order[:currency]
           xml.b :CUSTOMER_CODE
+          xml.b :DISCOUNT,           order[:item_discount] || 0
+          xml.b :SHIPPING_FEES,      shipping_fees(order)
+          xml.b :TAXES,              order[:tax] || 0
+          xml.b :TOTAL_AMOUNT,       order[:total_amount] || 0
           xml.b :DELIVERY_GIFT_WRAP, order[:giftwrap]
-          xml.b :DELIVERY_MESSAGE,   "<![CDATA[#{order[:gift_message]}]]>"
+          xml.b :DELIVERY_MESSAGE do
+            xml.cdata!(order[:gift_message])
+          end
           xml.b :EMAIL,              order[:email]
           xml.b :ORDER_ID,           order[:number]
-          xml.b :ORDER_TYPE,         order[:type] || "OO"
+          xml.b :ORDER_TYPE,         order[:type] || 'OO'
           xml.b :DELIVERY_FROM,   :"i:nil" => "true"
           xml.b :DELIVERY_TO,     :"i:nil" => "true"
           xml.b :DELIVERY_ID,     :"i:nil" => "true"
@@ -30,10 +39,18 @@ module Stellae
 
     def build_address(xml, type, address)
       full_name = "#{address[:first_name]} #{address[:last_name]}"
-      xml.b :"#{type}_NAME",            "<![CDATA[#{full_name}]]>"
-      xml.b :"#{type}_ADDRESS_1",       "<![CDATA[#{(address[:address1])}]]>"
-      xml.b :"#{type}_ADDRESS_2",       "<![CDATA[#{(address[:address2])}]]>"
-      xml.b :"#{type}_ADDRESS_3",       "<![CDATA[#{(address[:address3])}]]>"
+      xml.b :"#{type}_NAME" do
+        xml.cdata!(full_name)
+      end
+      xml.b :"#{type}_ADDRESS_1" do
+        xml.cdata!(address[:address1])
+      end
+      xml.b :"#{type}_ADDRESS_2" do
+        xml.cdata!(address[:address2])
+      end
+      xml.b :"#{type}_ADDRESS_3" do
+        xml.cdata!(address[:address3])
+      end
       xml.b :"#{type}_ADDRESS_CITY",    address[:city]
       xml.b :"#{type}_ADDRESS_COUNTRY", address[:country]
       xml.b :"#{type}_ADDRESS_STATE",   address[:state]
@@ -48,10 +65,22 @@ module Stellae
             xml.b :PRICE,    line_item[:price]
             xml.b :QUANTITY, line_item[:quantity]
             xml.b :SIZE,     line_item[:size]
-            xml.b :sku,      line_item[:sku]
+            xml.b :SKU,      line_item[:sku]
           end
         end
       end
+    end
+
+    def shipping_cost(order)
+      order[:shipping_cost] || 0
+    end
+
+    def shipping_discount(order)
+      order[:shipping_discount] || 0
+    end
+
+    def shipping_fees(order)
+      shipping_cost(order).abs - shipping_discount(order).abs
     end
 
   end
